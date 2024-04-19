4118f72433ef25e96ba918048c6f5fc7f180b614
resort-io <nicholas.macnutt@hotmail.com>
2024-04-19 19:55:27 -0300

    Refactored `main` method. Added Config `version` static variable.

--- src/__init__.py
+++ src/__init__.py
@@ -1,7 +1,6 @@
 import json
 import logging
 import os
-import requests
 import sched
 import time
 from config import Config
@@ -31,58 +30,42 @@ def main() -> None:
         Config.update_and_verify()
 
         interval = Config.get_watcher_interval()
-
         rss = Config.get_nyaa_rss()
         watchlist = Config.get_watcher_watchlist()
         history = Config.get_watcher_history()
+        webhooks = Config.get_discord_webhooks()
+
         watcher = Watcher(rss, watchlist, history)
+        webhook = Webhook(webhooks)
 
-        discord_webhooks = Config.get_discord_webhooks()
-        webhook = Webhook(discord_webhooks)
+        Logger.debug(
+            f"INTERVAL: {interval} seconds.\n"
+            f"NYAA RSS: {rss}\n"
+            f"WATCHLIST: {len(watchlist.get('watchlist'))} entr{'y' if len(watchlist.get('watchlist')) == 1 else 'ies'}.\n"
+            f"HISTORY: {len(history.get('downloads'))} download(s) and {len(history.get('errors'))} error(s).\n"
+            f"WEBHOOKS: {len(webhook.get_json_webhooks().get('webhooks'))} entr{'y' if len(webhook.get_json_webhooks().get('webhooks')) == 1 else 'ies'}."
+        )
+        Logger.log(f"Done! Watcher started (v{Config.version}).")
 
-        Logger.debug(f"INTERVAL: {interval} seconds.\n"
-                     f"NYAA RSS: {rss}\n"
-                     f"WATCHLIST: {len(watchlist.get('watchlist'))} entr{'y' if len(watchlist.get('watchlist')) == 1 else 'ies'}.\n"
-                     f"HISTORY: {len(history.get('downloads'))} download(s) and {len(history.get('errors'))} error(s).\n"
-                     f"WEBHOOKS: {len(webhook.get_json_webhooks().get('webhooks'))} entr{'y' if len(webhook.get_json_webhooks().get('webhooks')) == 1 else 'ies'}.")
-    except json.decoder.JSONDecodeError as e:
-        line = e.doc.split("\n")[e.lineno - 1].replace("  ", "")
-        message = "'" + e.msg + "'" if '.json' in e.msg else 'JSON'
-        Logger.log(f"Parse Error: {message} syntax error found on line {e.lineno} at column {e.pos}.\n"
-                   f"    {line}\nWatcher exited.", {"white_lines": "b"})
-        Logger.debug(f"{e}", {"exc_info": True})
-        exit(-1)
-    except Exception as e:
-        Logger.log(f"{e}\nWatcher exited.", {"white_lines": "b"})
-        Logger.debug(f"{e}", {"exc_info": True})
-        exit(-1)
-
-    Logger.log("Connecting to RSS...")
-    try:
-        response = requests.get(rss, timeout=60)
-
-        if response.status_code != 200:
-            Logger.log(f"Connection Error: Could not read RSS URL; received HTTPS Status Code: {str(response.status_code)}. "
-                       f"Add a valid Nyaa RSS URL to config.json and restart the server.\nWatcher exited.", {"white_lines": "b"})
-            exit(-1)
-    except Exception as e:
-        Logger.log("Connection Error: Cannot connect to RSS URL. Your internet provider could be blocking requests to nyaa domains."
-                   f"\n{e}\nWatcher exited.", {"exc_info": True, "white_lines": "b"})
-        exit(-1)
-    Logger.debug(f"Connection successful. HTTPS Status Code: {str(response.status_code)}.")
-
-    Logger.log("Done! Watcher started.")
-    try:
         scheduler = sched.scheduler(time.time, time.sleep)
         scheduler.enter(1, 1, fetch, (scheduler, watcher, interval, webhook))
         scheduler.run()
+
+    except json.decoder.JSONDecodeError as e:
+        line = e.doc.split("\n")[e.lineno - 1].replace("  ", "")
+        message = "'" + e.msg + "'" if '.json' in e.msg else 'JSON'
+
+        Logger.log(f"Parse Error: {message} syntax error found on line {e.lineno} at column {e.pos}.\n"
+                   f"    {line}\nWatcher exited.", {"white_lines": "b"})
+        Logger.debug(f"{e}", {"exc_info": True})
+        exit(1)
     except KeyboardInterrupt:
         Logger.log("Watcher exited.", {"white_lines": "bt"})
         exit(0)
     except Exception as e:
-        Logger.log(f"{e}\nWatcher exited.", {"white_lines": "bt"})
+        Logger.log(f"{e}\nWatcher exited.", {"white_lines": "b"})
         Logger.debug(f"{e}", {"exc_info": True})
-        exit(-1)
+        exit(1)
 
 
 if __name__ == "__main__":
