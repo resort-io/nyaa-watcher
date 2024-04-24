042f09704e850f2c317cd728b6b68e481ae2b82a
resort-io <nicholas.macnutt@hotmail.com>
2024-04-24 04:20:08 -0300

    Added Python docstrings to `watcher.py`.

--- src/watcher.py
+++ src/watcher.py
@@ -6,6 +6,15 @@ from config import Config
 
 
 def _sort_torrents(torrents: list) -> list:
+    """
+    Sorts a list of torrents based on their titles.
+
+    Args:
+        torrents (list): A list of dictionaries, each representing an RSS torrent entry.
+
+    Returns:
+        list: A list of sorted torrent dictionaries, ordered by their 'title' key in ascending order.
+    """
     torrent_titles = [(torrent['title'], torrent) for torrent in torrents]
     torrent_titles.sort()
     return [pair[1] for pair in torrent_titles]
@@ -18,6 +27,20 @@ class Watcher:
         self.history = history_json
 
     def append_to_history(self, torrents: list) -> None:
+        """
+        Appends a list of torrents to the history.
+
+        Args:
+            torrents (list): A list of dictionaries, each representing a torrent. Each dictionary must have the following keys:
+                - 'uploader': The name of the uploader.
+                - 'title': The title of the torrent.
+                - 'download_datetime': The date and time when the torrent was downloaded.
+                - 'id': The URL of the torrent.
+                - 'nyaa_infohash': The hash of the torrent given by Nyaa.
+
+        Returns:
+            None
+        """
         for torrent in torrents:
             self.history.get('downloads').append({
                 "uploader": torrent.get('uploader'),
@@ -28,6 +51,18 @@ class Watcher:
             })
 
     def fetch_feed(self, rss: str, watchlist: dict, sub_name: str = None, prev_hash: str = None) -> list:
+        """
+        Fetches an RSS feed and filters the torrents based on the watchlist.
+
+        Args:
+            rss (str): The URL of the RSS feed.
+            watchlist (dict): A dictionary containing the watchlist entries.
+            sub_name (str, optional): The name of the subscription. Defaults to None.
+            prev_hash (str, optional): The last torrent hash of the previous fetch. Defaults to None.
+
+        Returns:
+            list: A list of dictionaries, each representing a torrent to be downloaded from a subscription.
+        """
         log_entries = os.environ.get("LOG_RSS_ENTRIES", "false").lower() == "true"
 
         feed = feedparser.parse(rss)
@@ -112,6 +147,12 @@ class Watcher:
         return _sort_torrents(queue)
 
     def fetch_all_feeds(self) -> list:
+        """
+        Fetches all RSS feeds from the subscriptions and filters the torrents based on the watchlist.
+
+        Returns:
+            list: A list of dictionaries, each representing a torrent to be downloaded from all subscriptions.
+        """
         queue = []
         Logger.log()
         for sub in self.subscriptions.get("subscriptions"):
