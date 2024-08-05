ac24cb0772464fa51855f7ea6228396842e39bbd
resort-io <nicholas.macnutt@hotmail.com>
2024-08-05 19:24:04 -0300

    Fixed bug where regex was not properly matching torrent titles.

--- src/watcher.py
+++ src/watcher.py
@@ -2,6 +2,7 @@ import feedparser
 import os
 import re
 from config import Config
+from feedparser import FeedParserDict
 from logger import Logger
 
 
@@ -23,7 +24,7 @@ class Watcher:
         self.subscriptions = subscriptions_json
         self.history = history_json
 
-    def append_to_history(self, torrents: list) -> None:
+    def append_to_history(self, torrents: list[dict]) -> None:
         """
         Appends a list of torrents to the history.
         :param torrents: A list of dictionaries, each representing a torrent. Each dictionary must have the following keys:
@@ -35,8 +36,10 @@ class Watcher:
         :return: None
         """
 
+        downloads: list[dict] = self.history.get('downloads')
+
         for torrent in torrents:
-            self.history.get('downloads').append({
+            downloads.append({
                 "uploader": torrent.get('uploader'),
                 "torrent_title": torrent.get('title'),
                 "date_downloaded": torrent.get('download_datetime'),
@@ -44,47 +47,48 @@ class Watcher:
                 "nyaa_hash": torrent.get('nyaa_infohash')
             })
 
-    def fetch_feed(self, rss: str, watchlist: dict, sub_name: str = None, prev_hash: str = None) -> list:
+    def fetch_feed(self, rss: str, watchlist: list[dict], sub_name: str, prev_hash: str = None) -> list:
         """
         Fetches an RSS feed and filters the torrents based on the watchlist.
-        :param rss: The URL of the RSS feed.
-        :param watchlist: A dictionary containing the watchlist entries.
-        :param sub_name: The name of the subscription (Defaults to None).
-        :param prev_hash: The last torrent hash of the previous fetch (Defaults to None).
-        :return: A list of dictionaries, each representing a torrents from a given subscription to be downloaded.
+        :param rss: The URL of the RSS feed (E.g., https://nyaa.si/?page=rss&u=Username).
+        :param watchlist: The `watchlist` property from a `subscriptions` entry.
+        :param sub_name: The `username` property from a `subscriptions` entry.
+        :param prev_hash: The most recent torrent hash of the previous fetch. The `previous_hash` property from a `subscriptions` entry. (Defaults to None).
+        :return: A list of dictionaries, containing matched torrents fetched from the `rss` param.
         """
 
-        log_entries = os.environ.get("LOG_RSS_ENTRIES", "false").lower() == "true"
+        log_entries: bool = os.environ.get("LOG_RSS_ENTRIES", "false").lower() == "true"
+        feed: FeedParserDict = feedparser.parse(rss)
 
-        feed = feedparser.parse(rss)
         if len(feed.entries) == 0:
-            Logger.log(f"Cannot retrieve entries from {rss}.")
+            Logger.log(f"Unknown Error: No entries from {rss}.")
             return []
 
-        queue = []
+        download_queue = []
         for torrent in feed.entries:
-            title = torrent.get('title')
-            torrent_hash = torrent.get('nyaa_infohash')
+            title: str = torrent.get('title')
+            torrent_hash: str = torrent.get('nyaa_infohash')
+
+            # Check if the torrent file has fetched previously
+            if torrent_hash == prev_hash:
+                Logger.debug(f"Found previously fetched torrent: {title}")
+                break
 
             if log_entries:
                 Logger.debug(f"Torrent: {title}")
-
-            # Check if the following torrents have already been fetched
-            if torrent_hash == prev_hash:
-                break
 
             for watchlist_entry in watchlist:
                 tag_match = regex_match = ex_regex_match = hash_match = False
 
                 # Checking tags
-                tags = watchlist_entry.get("tags", [])
+                tags: list[str] = watchlist_entry.get("tags", [])
                 for tag in tags:
                     if tag.lower() in title.lower():
                         tag_match = True
                         break
 
                 # Checking regex
-                regexes = watchlist_entry.get("regex", [])
+                regexes: list[str] = watchlist_entry.get("regex", [])
                 for regex_pattern in regexes:
                     pattern = re.compile(regex_pattern)
                     if re.search(pattern, title):
@@ -92,16 +96,18 @@ class Watcher:
                         break
 
                 # Checking excluded regex
-                ex_regexes = watchlist_entry.get("exclude_regex", [])
-                for regex_pattern in regexes:
+                ex_regexes: list[str] = watchlist_entry.get("exclude_regex", [])
+                for regex_pattern in ex_regexes:
                     pattern = re.compile(regex_pattern)
                     if re.search(pattern, title):
                         ex_regex_match = True
                         break
 
-                match = tag_match and regex_match or len(tags) == 0 and regex_match or tag_match and len(regexes) == 0
+                match: bool = (tag_match or not tags) and (regex_match or not regexes)
+                if ex_regex_match:
+                    match = False
 
-                # Checking hash in successful downloads
+                # Checking if torrent has been downloaded
                 if match:
                     history_entry = [(entry['nyaa_hash'], entry) for entry in self.history.get("downloads") if entry.get('nyaa_hash') == torrent_hash]
                     hash_match = len(history_entry) > 0
@@ -109,10 +115,10 @@ class Watcher:
                 if log_entries:
                     Logger.debug(
                         f"Watchlist: {watchlist_entry.get('name', 'Unknown Watchlist')}\n"
-                        f" - Tags     (Match={tag_match}): {tags}\n"
-                        f" - RegEx    (Match={regex_match}): {regexes}\n"
-                        f" - Ex.RegEx (Match={ex_regex_match}): {ex_regexes}\n"
-                        f" - Hash     (Match={hash_match}): {torrent_hash}"
+                        f" - Tags     (Match = {tag_match}): {tags}\n"
+                        f" - RegEx    (Match = {regex_match}): {regexes}\n"
+                        f" - Ex.RegEx (Match = {ex_regex_match}): {ex_regexes}\n"
+                        f" - Hash     (Match = {hash_match}): {torrent_hash}"
                     )
                     if watchlist_entry == watchlist[-1]:
                         Logger.debug()
@@ -122,20 +128,19 @@ class Watcher:
                     torrent['webhooks'] = watchlist_entry.get("webhooks", [])
                     torrent['uploader'] = sub_name
                     torrent['watchlist'] = watchlist_entry.get('name', "Unknown Watchlist")
-                    queue.append(torrent)
+                    download_queue.append(torrent)
 
                     if log_entries:
-                        Logger.debug("Torrent added to download list.")
+                        Logger.debug("Torrent added to download queue.")
                     break
 
-        if sub_name:
-            Config.set_previous_hash(sub_name, feed.entries[0].get('nyaa_infohash', ""))
-        return _sort_torrents(queue)
+        Config.set_previous_hash(sub_name, feed.entries[0].get('nyaa_infohash', ""))
+        return _sort_torrents(download_queue)
 
     def fetch_all_feeds(self) -> list:
         """
         Fetches all RSS feeds from the subscriptions and filters the torrents based on the watchlist.
-        :return: A list of dictionaries, each representing a torrents from all subscriptions to be downloaded.
+        :return: A list of dictionaries, containing matched torrents fetched from all subscriptions.
         """
 
         queue = []
